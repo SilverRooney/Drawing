0515c487e7a6380b8b0e3812756ab02df2ad90ac
SilverRooney <silver.rooney.dev@gmail.com>
2022-06-15 10:48:36 -0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Silver on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
